44fd9fd41fa17e198c900ccb1d20403bd637ff3a
MZ <maozhou3@163.com>
2017-04-17 14:18:23 +0800

    first commit

---
+++ simplerecyclerviewlib/src/main/java/com/github/blazecake/simprecyclerviewlibrary/SimpleRecyclerView.java
@@ -0,0 +1,92 @@
+package com.github.blazecake.simprecyclerviewlibrary;
+
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.AttributeSet;
+
+/**
+ * Created by MZ on 2017/4/17.
+ */
+
+public class SimpleRecyclerView extends RecyclerView {
+
+    private boolean isLoadable=true; //是否可加载更多
+    private boolean loading=false;
+    private int previousTotal=0;
+    private LinearLayoutManager layoutManager;
+
+    private OnLoadMoreListener onLoadMoreListener;
+
+    public void setLoadable(boolean loadable) {
+        isLoadable = loadable;
+    }
+
+    public void setLayoutManager(LinearLayoutManager layoutManager) {
+        this.layoutManager = layoutManager;
+        setLayoutManager(layoutManager);
+    }
+
+    public void setOnLoadMoreListener(OnLoadMoreListener onLoadMoreListener) {
+        this.onLoadMoreListener = onLoadMoreListener;
+    }
+
+    public SimpleRecyclerView(Context context) {
+        super(context);
+    }
+
+    public SimpleRecyclerView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SimpleRecyclerView(Context context, @Nullable AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    private void init(){
+        layoutManager = new LinearLayoutManager(getContext());
+        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        setLayoutManager(layoutManager);
+
+        setHasFixedSize(true);
+        setItemAnimator(new DefaultItemAnimator());
+
+        addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+            }
+
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+                if(isLoadable) {
+                    int lastVisibleItem = layoutManager.findLastVisibleItemPosition();
+                    int totalItemCount = layoutManager.getItemCount();
+                    if (loading) {
+                        if (totalItemCount > previousTotal) {
+                            loading = false;
+                            previousTotal = totalItemCount;
+                        }
+                    }else{
+                        previousTotal = totalItemCount;
+                    }
+                    if (!loading && lastVisibleItem >= totalItemCount - 1) {
+                        loading=true;
+                        if(onLoadMoreListener!=null){
+                            onLoadMoreListener.onLoadMore();
+                        }
+                    }
+                }
+            }
+        });
+    }
+
+    public interface OnLoadMoreListener{
+        void onLoadMore();
+    }
+
+}
